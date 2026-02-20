bdca3cde83437fd793cf85ab776262f22644cf7a
Frici <friedrichvirgil@gmail.com>
2026-02-20 22:53:28 +0200

    init: Setup the api and a couple testing facts

---
+++ api/facts.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
+facts = [
+	"Wood frogs can freeze up to 70% of their body water to survive cold temperatures. They just thaw out in the spring.",
+	"Many fish can change their sexes, generally triggered by temperature changes or sex ratio in the local population",
+	"Herping is the amphibian/reptile (herptile) equivalent to birding",
+	"Crows can recognize human faces for years and pass down the knowledge through generations",
+	"Male anglerfish get absorbed by the female angler fish after mating"
+]
+
+@app.get("/api/facts")
+def get_random_fact():
+    return {"fact": random.choice(facts)}
+
